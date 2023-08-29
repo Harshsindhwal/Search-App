fe16ef3ea9fc76ab112dc538350834545b9c892d
Harsh Sindhwal <101384145+Harshsindhwal@users.noreply.github.com>
2023-08-29 21:55:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const formEL = document.querySelector("form")
 const inputEl = document.getElementById("search-input")
 const searchResults = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
+
+function searchImages(){
+    inputData = inputEl.value;
+}
